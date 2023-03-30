3d7565c9ffe5120085d2fcb12690544950eadaef
sy <sy@user.noreply.github.com>
2023-03-30 20:31:53 +0800

    feat: gin server

---
+++ interface/http/server.go
@@ -0,0 +1,77 @@
+package http
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"go-web/interface/http/middleware"
+
+	"github.com/gin-contrib/gzip"
+	gin_zap "github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+	"github.com/google/wire"
+	"go.uber.org/zap"
+)
+
+var ProviderSet = wire.NewSet(NewServer, NewRouter)
+
+type Server struct {
+	logger     *zap.Logger
+	router     *gin.Engine
+	httpServer *http.Server
+}
+
+const closeWaitTime = 30 * time.Second
+
+type InitRoutersFunc func(r *gin.Engine)
+
+func NewRouter(logger *zap.Logger, initRoutersFunc InitRoutersFunc) *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.New()
+
+	r.Use(gzip.Gzip(gzip.DefaultCompression, gzip.WithExcludedExtensions([]string{})))
+	r.Use(gin_zap.Ginzap(logger, time.RFC3339, true))
+	r.Use(gin_zap.RecoveryWithZap(logger, true))
+	r.Use(middleware.Cors())
+
+	initRoutersFunc(r)
+
+	return r
+}
+
+func NewServer(logger *zap.Logger, router *gin.Engine) *Server {
+	srv := &http.Server{
+		Addr:              os.Getenv("HTTP_SERVER_PORT"),
+		Handler:           router,
+		ReadHeaderTimeout: 20 * time.Second,
+	}
+
+	return &Server{
+		logger:     logger.With(zap.String("component", "http_server")),
+		router:     router,
+		httpServer: srv,
+	}
+}
+
+func (server *Server) StartServer() {
+	go func() {
+		if err := server.httpServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			server.logger.Fatal("http server listen and serve error", zap.Error(err))
+		}
+	}()
+}
+
+func (server *Server) StopServer() error {
+	ctx, cancel := context.WithTimeout(context.Background(), closeWaitTime)
+	defer cancel()
+	if err := server.httpServer.Shutdown(ctx); err != nil {
+		return fmt.Errorf("http server shutdown error: %w", err)
+	}
+
+	return nil
+}
